56cc8cb074d4471b5845e0a2ac94c17963eb1d49
Cosmin <126340735+DragusanuCosmin@users.noreply.github.com>
2023-08-08 10:03:15 +0300

    kafkachanges

--- src/main/java/com/example/judupdater/Listener/KafkaMessageListener.java
+++ src/main/java/com/example/judupdater/Listener/KafkaMessageListener.java
@@ -1,37 +1,20 @@
 package com.example.judupdater.Listener;
 
 import com.example.judupdater.Email.EmailService;
-import com.example.judupdater.Entities.Clienti;
-import com.example.judupdater.Entities.Dosare;
-import com.example.judupdater.Service.ClientiService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @Component
 public class KafkaMessageListener {
-    private Dosare previousDosare;
-    private final EmailService emailService;
-    private final ClientiService clientiService;
-
-    public KafkaMessageListener(EmailService emailService, ClientiService clientiService) {
-        this.emailService = emailService;
-        this.clientiService = clientiService;
+    @Autowired
+    private EmailService emailSenderService;
+    //@Autowired
+    //private SearchDosarOwner searchDosarOwner;
+    @KafkaListener(topics = "dosare_noi", groupId = "test-consumer-group")
+    public void listen(String in) {
+        System.out.println("S-a introdus dosarul nr: " + in);
+        emailSenderService.sendMessage("luci@ctce.ro" , "Noutati in dosarul" + in, "Modificari in dosarul" + in);
+      //  searchDosarOwner.sendEmailForDosar(in);
     }
 
-    @KafkaListener(topics = "dosare_noi", containerFactory = "kafkaListenerContainerFactory", groupId = "group_id")
-    public void receiveMessage(Dosare dosare) {
-        if (previousDosare != null && !Objects.equals(dosare.getDetaliiDosar(), previousDosare != null ? previousDosare.getDetaliiDosar() : null)) {
-            System.out.println("Mesaj schimbat. Se trimite email");
-            Clienti client = clientiService.getClientById(dosare.getIdClient());
-
-            if (client != null) {
-                emailService.sendMessage(client.getEmail(), "Schimbare dosar", "Dosarul dumneavoastra a fost schimbat: " + dosare.getDetaliiDosar());
-            } else {
-                System.out.println("Client invalid: " + dosare.getIdClient());
-            }
-        }
-        previousDosare = dosare;
-    }
 }
---
+++ src/main/java/com/example/judupdater/Service/KafkaConsumerService.java
@@ -0,0 +1,16 @@
+//package com.example.judupdater.Service;
+//
+//import com.example.judupdater.Entities.Dosare;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//@Slf4j
+//@Service
+//public class KafkaConsumerService {
+//
+//    @KafkaListener(id="idu_meu1",topics = "dosare_noi")
+//    public void receiveDosare(Dosare dosare) {
+//        log.info("Received Dosare: " + dosare);
+//    }
+//}
