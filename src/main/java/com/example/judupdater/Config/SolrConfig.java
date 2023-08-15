d4c17eacbdd2b112d93572e4f593fb845e8d7e52
Cosmin <126340735+DragusanuCosmin@users.noreply.github.com>
2023-08-15 20:12:49 +0300

    dependencies updates

--- src/main/java/com/example/judupdater/Config/SolrConfig.java
+++ src/main/java/com/example/judupdater/Config/SolrConfig.java
@@ -9,18 +9,17 @@ import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @Configuration
-@EnableSolrRepositories
-
+@EnableSolrRepositories(basePackages = "com.example.judupdater.Repository", namedQueriesLocation = "classpath:solr-named-queries.properties")
 @ComponentScan
 public class SolrConfig {
 
     @Bean
     public SolrClient solrClient() {
-        return new HttpSolrClient.Builder("http://192.168.3.39:8984/solr").build();
+        return new HttpSolrClient.Builder("http://localhost:8983/solr").build();
     }
 
     @Bean
-    public SolrTemplate solrTemplate(SolrClient client){
+    public SolrTemplate solrTemplate(SolrClient client) throws Exception {
         return new SolrTemplate(client);
     }
 }
--- src/main/java/com/example/judupdater/Repository/DosarSolrRepository.java
+++ src/main/java/com/example/judupdater/Repository/DosarSolrRepository.java
@@ -1,21 +1,21 @@
-package com.example.judupdater.Repository;
-
-import com.example.judupdater.Entities.DosareSolr;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.solr.repository.Query;
-import org.springframework.data.solr.repository.SolrCrudRepository;
-
-import java.util.List;
-
-public interface DosarSolrRepository extends SolrCrudRepository<DosareSolr, String> {
-
-    public List<DosareSolr> findById(String id);
-
-    @Query("id:*?0* OR name:*?0*")
-    public Page<DosareSolr> findByCustomQuery(String searchTerm, Pageable pageable);
-
-    @Query(name = "Product.findByNamedQuery")
-    public Page<DosareSolr> findByNamedQuery(String searchTerm, Pageable pageable);
-
-}
+//package com.example.judupdater.Repository;
+//
+//import com.example.judupdater.Entities.DosareSolr;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.solr.repository.Query;
+//import org.springframework.data.solr.repository.SolrCrudRepository;
+//
+//import java.util.List;
+//
+//public interface DosarSolrRepository extends SolrCrudRepository<DosareSolr, String> {
+//
+//    public List<DosareSolr> findById(String id);
+//
+//    @Query("id:*?0* OR name:*?0*")
+//    public Page<DosareSolr> findByCustomQuery(String searchTerm, Pageable pageable);
+//
+//    @Query(name = "Product.findByNamedQuery")
+//    public Page<DosareSolr> findByNamedQuery(String searchTerm, Pageable pageable);
+//
+//}
